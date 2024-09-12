68801b488e58e8dfb8b9f94388c6d03fabaf7c32
n0228 <nlnrao228@gmail.com>
2024-09-12 12:53:58 +0530

    Updated projects

--- doc/src/templates/react/Components/Breadcrumb/components/SampleCode.js
+++ doc/src/templates/react/Components/Breadcrumb/components/SampleCode.js
@@ -3,7 +3,10 @@ import { Breadcrumb, Colors } from 'e-ui-react';
 
 export const SampleNote1 = () =>{
  return (<>
- <Breadcrumb backgroundColor={Colors.light} />
+ <Breadcrumb backgroundColor={Colors.light} 
+ data={[{ label:'Page1', url:'#', onClick:()=>alert('Page 01') },
+    { label:'Hello', url:'#', onClick:()=>alert('Hello') }
+ ]} />
  </>);
 };
 
