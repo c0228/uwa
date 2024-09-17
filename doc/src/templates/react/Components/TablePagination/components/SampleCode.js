b4bc1777e503734a5abd65af401430e75f08c01d
n0228 <nlnrao228@gmail.com>
2024-09-17 16:24:55 +0530

    Updated projects

--- doc/src/templates/react/Components/TablePagination/components/SampleCode.js
+++ doc/src/templates/react/Components/TablePagination/components/SampleCode.js
@@ -30,6 +30,39 @@ export const SampleNote1 = () =>{
 ];
 
  return (<>
+  <TablePagination mode="scrollable" header={{ backgroundColor:'blue', color:'#fff', columns: columns }} pageSize={5} apiUrl="http://localhost:8080/assets/apis/table.json" />
+  </>);
+};
+
+export const SampleNote2 = () =>{
+  const columns = [{
+    header: 'S.No.',
+    align:'center',
+    key: 'id',
+    width:'4%',
+    render: (_row, _searchTerm, rowIndex) => (<strong>{rowIndex+1}</strong>)
+  },
+  {
+    header: 'Column 1 Title',
+    key: 'headerColumn1',
+    width:'32%',
+    render: (row, searchTerm) =>(<strong>{searchTableTerm(row?.Column1, searchTerm)}</strong>)
+  },
+  { 
+    header: 'Column 2 Title', 
+    key: 'headerColumn2', 
+    width:'40%',
+    render: (row, searchTerm) =>(<strong>{searchTableTerm(row?.Column2, searchTerm)}</strong>)
+  },
+  { 
+    header: 'Column 3 Title', 
+    key: 'headerColumn3', 
+    width:'40%',
+    render: (row, searchTerm) =>(<strong>{row?.Column3+' (Non-Searchable)'}</strong>)
+  },
+];
+
+ return (<>
   <TablePagination header={{ backgroundColor:'blue', color:'#fff', columns: columns }} pageSize={10} apiUrl="http://localhost:8080/assets/apis/table.json" />
   </>);
 };
