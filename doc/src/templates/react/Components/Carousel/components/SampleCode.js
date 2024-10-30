d7d4f64f22243b66c1602337d0a63e7896fb4f8d
n0228 <nlnrao228@gmail.com>
2024-10-30 20:54:56 +0530

    Updated Projects

--- doc/src/templates/react/Components/Carousel/components/SampleCode.js
+++ doc/src/templates/react/Components/Carousel/components/SampleCode.js
@@ -7,30 +7,25 @@ export const SampleNote1 = () =>{
  return (<>
  <Carousel 
     id="listing" 
-    items={[{ 
-              image:'https://www.w3schools.com/bootstrap5/la.jpg', 
-              alt:'Los Angeles', 
-              caption:{ 
-                  title:'Los Angeles', 
-                  desc:'We had such a great time in LA!' 
-              } 
-            },
-            { 
-              image:'https://www.w3schools.com/bootstrap5/chicago.jpg', 
-              alt:'Chicago', 
-              caption:{ 
-                title:'Chicago', 
-                desc:'Thank you, Chicago!' 
-              } 
-            },
-            { 
-              image:'https://www.w3schools.com/bootstrap5/ny.jpg', 
-              alt:'New York', 
-              caption:{ 
-                title:'New York', 
-                desc:'We love the Big Apple!' 
-              } 
-            }]} />
+    data={[{
+          img: { src:'https://preview.colorlib.com/theme/bootstrap/carousel-01/images/slider-1.jpg', alt:'Los Angeles' },
+          content:(<div style={{ color:'#fff' }}>
+                      <h2>Slide 1</h2>
+                      <p>This is the One slide.</p>
+                  </div>)
+     },{
+         img:{ src:'https://www.josephfiler.com/images/xl/Arizona-Desert-Mountains-2518-Edit.jpg', alt:'Chicago' },
+         content:(<div style={{ color:'#fff' }}>
+                    <h2>Slide 2</h2>
+                    <p>This is the Two slide.</p>
+                  </div>)
+     },{
+         img:{ src:'https://wallpapers.com/images/hd/beautiful-ocean-pictures-1920-x-1200-6r2ljotdu3o25cio.jpg', alt:'New York' },
+         content:(<div style={{ color:'#fff' }}>
+                    <h2>Slide 3</h2>
+                    <p>This is the Third slide.</p>
+                  </div>)
+     }]} />
  </>);
 
 };
--- doc/src/templates/react/Components/Carousel/index.js
+++ doc/src/templates/react/Components/Carousel/index.js
@@ -13,7 +13,7 @@ const CarouselNotes = () => {
       <Card padding={15} backgroundColor={Colors.grey}>
         <div className="mbot10p"><b>Sample Code:</b></div>
         <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['1']}  />
-        <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['7T32']}  />
+        <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['7T27']}  />
         <div align="center"><b>SampleCode.js</b></div>
         <div className="mtop10p mbot10p"><b>Output:</b></div>
         <SampleNote1 />
