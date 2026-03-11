f9b76dae454db79e5ca59233dd65a0e5d24d5152
NLNRao <nlnrao228>
2026-03-11 19:57:19 +0530

    Updated projects

--- doc/src/templates/react/Components/Toast/components/SampleCode.js
+++ doc/src/templates/react/Components/Toast/components/SampleCode.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
-import { Toast, GetCurrentTimeStamp } from 'e-ui-react';
+import { Toast, Select, Button } from 'e-ui-react';
 
 export const SampleNote1 = () =>{
 
- const ToastExample = ()=>{
+ const ToastExample = () => {
+  const data = ["top-right", "top-left", "top-center", "middle-center", "middle-left", "middle-right", "bottom-right", "bottom-left", "bottom-center"];
+  const [show, setShow] = useState(false);
+  const [ selected, setSelected ] = useState();
+  return (
+    <div className="d-flex gap-2">
+       <Select
+         placeholder="Select Position"
+         options={data?.map(d=>({ id: d, label: d, value: d }))}
+         width="130" 
+         fontSize="12"
+         onChange={(event) => {
+            setSelected(event.target.value);
+          }}
+         />
+      <Button type="primary" size={11}
+        onClick={() => setShow(true)}
+      >
+        View Toast
+      </Button>
 
-  const [ toastList, setToastList ] = useState([]);
-  
-  const toastData = { title:'Bootstrap', message:'Hello, world! This is a toast message.', timestamp: GetCurrentTimeStamp() };
-
-  return (<>
-   <button type="button" className="btn btn-primary" id="liveToastBtn" onClick={()=>setToastList([...toastList, toastData])}>View Toast</button>
-   <Toast data={toastList} />
-  </>);
-  
+      <Toast
+        position={selected}
+        show={show}
+        message="Hello, world! This is a toast message."
+        onClose={() => setShow(false)}
+      />
+    </div>
+  ); 
  };
 
  return (<ToastExample />);
