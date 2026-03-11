df3b6f2354fa1fbbebb27fa0839dbefadad8f8d9
NLNRao <nlnrao228>
2026-03-11 20:59:44 +0530

    Updated projects

--- doc/src/templates/react/Components/Modal/components/SampleCode.js
+++ doc/src/templates/react/Components/Modal/components/SampleCode.js
@@ -4,21 +4,23 @@ import { Modal, Button } from 'e-ui-react';
 export const SampleNote1 = () =>{
  
  const SimpleModal = ()=>{
+  const [ showModel, setShowModal ] = useState(false);
+
+  const HelloWorld = ()=>{
+    return (<div style={{ padding:'15px' }}>Hello World</div>);
+  };
   
-   const [ showModel, setShowModal ] = useState(false);
-
-   const HelloWorld = ()=>{
-    return (<div>Hello World</div>);
-   };
-
-   return (<>
+  return (<>
     <Button type="primary" size={11} onClick={()=>setShowModal(true)}>Open Simple Model</Button>
-    <Modal title="Modal Header" show={showModel} onClose={setShowModal}>
+    <Modal title="Modal Header" show={showModel} onClose={setShowModal} 
+      colorConfig={{
+        header: { backgroundColor:'red', color:'white', borderColor:'white' },
+        body: { backgroundColor:'blue', color:'yellow', borderColor:'orange' }
+      }}>
      <HelloWorld />
     </Modal>
-   </>);
-
-  };
+  </>);
+ };
 
   return (<SimpleModal />);
 
--- doc/src/templates/react/Components/Modal/index.js
+++ doc/src/templates/react/Components/Modal/index.js
@@ -13,7 +13,7 @@ console.log(SampleCodeJS.toString());
   <Card padding={15} backgroundColor={Colors.grey}>
   <div className="mbot10p"><b>Sample Code:</b></div>
   <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['1']}  />
-  <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['5T18']}  />
+  <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['5T23']}  />
   <div className="mtop10p mbot10p"><b>Output:</b></div>
   <SampleNote1 />
   </Card>
