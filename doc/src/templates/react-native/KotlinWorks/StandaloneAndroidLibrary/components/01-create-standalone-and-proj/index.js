1d38fe7b1f600a234dbdd156f15b8c1dd4c623f9
NLNRao <nlnrao228>
2026-06-03 13:20:16 +0530

    Updated projects

--- doc/src/templates/react-native/KotlinWorks/StandaloneAndroidLibrary/components/01-create-standalone-and-proj/index.js
+++ doc/src/templates/react-native/KotlinWorks/StandaloneAndroidLibrary/components/01-create-standalone-and-proj/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TreeView } from "e-ui-react";
+import { ContainerFluid, Row, Col, TreeView, Highlight } from "e-ui-react";
 
 const AND_PROJECT_STRUCTURE = [{
     id:'aar-project-name',
@@ -8,9 +8,72 @@ const AND_PROJECT_STRUCTURE = [{
     open: true,
 }];
 
+const CMD_BUILDS_GENERATE = `1. Application
+2. Library
+3. Gradle Plugin
+4. Basic (build structure only)`;
+
+const CMD_IMPL_LANG = `1. Java
+2. Kotlin
+3. Groovy
+4. Scala
+5. C++
+6. Swift`;
+
+const CMD_BUILD_DSL = `1. Kotlin
+2. Groovy`;
+
+const CMD_TEST_FW = `1. Kotlin.test
+2. Junit Jupiter`;
+
 const CreateStandaloneAndroidProject = () =>{
  return (<div>
-    <TreeView data={AND_PROJECT_STRUCTURE} />
+    <ContainerFluid>
+        <Row>
+            <Col md={6}>
+                <ol>
+                    <li>
+                        <div className="mtop5p">Create a Standalone Android Library (.aar) using following command-</div>
+                        <div className="mtop5p"><Highlight content={`gradle init`} lang="java" /></div>
+                    </li>
+                    <li>
+                        <div className="mtop5p">Types of builds to generate: </div>
+                        <div className="mtop5p"><Highlight content={CMD_BUILDS_GENERATE} lang="java" /></div>
+                        <div>Choose <code><b>Library</b></code> for building Standaline Android Library.</div>
+                    </li>
+                    <li>
+                        <div className="mtop5p">Select implementation language:</div>
+                        <div className="mtop5p"><Highlight content={CMD_IMPL_LANG} lang="java" /></div>
+                        <div className="mtop5p">Choose <code><b>kotlin</b></code> as a Implementation Language.</div>
+                    </li>
+                    <li className="mtop5p">
+                        <div>Make Sure, we are using <code><b>Java Version = 17</b></code></div>
+                    </li>
+                    <li className="mtop5p">
+                        <div>Provide <code><b>Project name</b></code></div>
+                    </li>
+                    <li>
+                        <div className="mtop5p">Select build script DSL:</div>
+                        <div className="mtop5p"><Highlight content={CMD_BUILD_DSL} lang="java" /></div>
+                        <div className="mtop5p">Choose <code><b>Kotlin</b></code> as a build script DSL.</div>
+                    </li>
+                    <li className="mtop5p">
+                        <div className="mtop5p">Select test framework:</div>
+                        <div className="mtop5p"><Highlight content={CMD_TEST_FW} lang="java" /></div>
+                        <div className="mtop5p">Choose <code><b>Kotlin.test</b></code> as Test Framework.</div>
+                    </li>
+                    <li className="mtop5p">
+                       <div>Generate build using new APIs & behaviour (some features may change in the next minor release).</div>
+                    </li>
+                </ol>
+                <div className="mtop5p">This will generate Ready-made Startup Code for making <code><b>.aar</b></code> project.</div>
+            </Col>
+            <Col md={6}>
+                <div className="mtop5p">Ready-made Startup Code for making <code><b>.aar</b></code> project setup following Structure:</div>
+                <div className="mtop5p"><TreeView data={AND_PROJECT_STRUCTURE} /></div>
+            </Col>
+        </Row>
+    </ContainerFluid>
  </div>);
 };
 
--- doc/src/templates/react/SocialLogins/Facebook/index.js
+++ doc/src/templates/react/SocialLogins/Facebook/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ContainerFluid, Row, Col } from "e-ui-react";
+import { ContainerFluid, Row, Col, Card, Icon } from "e-ui-react";
 import { DocumentHeader } from "@DocUtils/DocHeaders.js";
 
 export const LoginFacebookPage = () =>{
@@ -10,9 +10,47 @@ export const LoginFacebookPage = () =>{
             <Row>
                 <Col md={12}>
                     <div><h5><b>Step 1: Create a Facebook App</b></h5></div>
-                    <div className="mtop5p">Go to: <a target="_blank" href="https://developers.facebook.com/">Meta for Developers</a></div>
+                    <div className="mtop5p">Go to: <a target="_blank" href="https://developers.facebook.com/apps/">Meta for Developers</a></div>
                     <ul>
-                        <li className="mtop5p"></li>
+                        <li className="mtop5p">
+                            <div>Click on <code><b>Create App</b></code></div>
+                            <Card padding={15}>
+                                <div><h5><b>1. App Details</b></h5></div>
+                                <div className="mtop5p padLeft15p">Provide <code><b>App Name</b></code> and <code><b>App Contact email</b></code></div>
+                                <div className="mtop15p"><h5><b>2. Use Cases</b></h5></div>
+                                <div className="mtop5p padLeft15p">Choose <code><b>Authenticate and request data from users using Facebook Login</b></code></div>
+                                <div className="mtop15p"><h5><b>3. Business</b></h5></div>
+                                <div className="padLeft15p">
+                                    <div className="mtop5p"><b>What is a Business Portfolio?</b></div>
+                                    <div className="mtop5p">
+                                        <div>A <b>Business Portfolio</b> (previously called <b><i>Meta Business Manager</i></b>) is an organization account 
+                                        that owns and manages:</div>
+                                        <ul>
+                                            <li className="mtop5p">Facebook Pages</li>
+                                            <li className="mtop5p">Instagram Accounts</li>
+                                            <li className="mtop5p">WhatsApp Business Accounts</li>
+                                            <li className="mtop5p">Apps</li>
+                                            <li className="mtop5p">Ad Accounts</li>
+                                            <li className="mtop5p">Team Members</li>
+                                            <li className="mtop5p">Permissions</li>
+                                        </ul>
+                                    </div>
+                                    <div className="mtop5p">
+                                        <div>By connecting a Business Portfolio, you can:</div>
+                                        <ul>
+                                            {["Use Facebook Login in production", "Access user profile data", "Access Instagram Graph API",
+                                            "Access Facebook Pages API", "Access WhatsApp Business APIs", "Submit apps for review",
+                                            "Publish the app publicly"]?.map((d,i)=>{
+                                                return (<li className="mtop5p">
+                                                    <Icon type="FontAwesome" name="fa-check-circle" color="green" style={{ marginRight:'5px'}} /> {d}
+                                                </li>);
+                                            })}
+                                        </ul>
+                                        <div className="mtop5p">Without verification, some permissions remain restricted.</div>
+                                    </div>
+                                </div>
+                            </Card>
+                        </li>
                         <li className="mtop5p"></li>
                     </ul>
                 </Col>
